d7b86e9b112c3986435b3adb09cc11748f7718c9
fede-dev <fedenriquez@hotmail.com>
2022-01-11 22:34:41 -0300

    add: mvd

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize("postgres://postgres@localhost/ecommerce", {
+//     logging: false,
+//     dialect: "postgres"
+// })
+
+// module.exports = sequelize
+
+// const { Sequelize } = require('sequelize');
+
+// const db = new Sequelize('postgres://postgres@localhost:5432/robert', {
+//     logging: false
+// })
+
+// module.exports = db
